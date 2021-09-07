b0347c0f4dc17e7d78d532b669a10ca0b94c1e46
marbleville <68442300+marbleville@users.noreply.github.com>
2021-09-07 17:03:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,12 @@ let charSetInv = [
     'h', 'a', 'o', '#', 'M', 'W', '&', '8',
     '%', 'B', '@', '$'];
 
-fixLine('abcdefg');
+const pixWidth = [3, 15, 15, 16, 12, 12, 9, 13, 13, 13, 14, 13, 13, 18, 18, 18, 9, 18, 12, 21, 16, 20, 17, 23, 23, 14, 14, 14, 14, 13, 14, 12, 12, 19, 26, 26, 21, 16, 26, 14, 25, 15];
+
+console.log(getPixWidth('$$$$$$$$$$Wu...................................uW$$$$$$$$$$$$$') + ' 1');
+console.log(getPixWidth('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$') + ' 2');
+
+console.log(pixWidth[charSetInv.indexOf('$')])
 
 bot.on('ready', () => {
     console.log('Bot Online');
@@ -65,27 +70,27 @@ function preprocess() {
             image.greyscale();
             image.contrast(0.5);
             image.scaleToFit(40, 50);
-            image.resize(image.bitmap.width, image.bitmap.height / 2);
+            image.resize(image.bitmap.width, image.bitmap.height / 3);
             image.write('pic2.png');
         }) 
 }
 
 function assembleMessage(message) {
-    let m = '';
+    let m = new Array();
     let l = '';
     Jimp.read('./pic2.png')
         .then(image => {
             image.scan(0, 0, image.bitmap.width, image.bitmap.height, function(x, y, idx) {
                 let index = Math.trunc((this.bitmap.data[idx] / 255) * (charSetInv.length - 1));
                     if (x == this.bitmap.width - 1) {
-                        m += `${fixLine(l + charSetInv[index])}\n`;
+                        m.push(`${fixLine(l + charSetInv[index])}`);
                         l = '';
                     }
                     else {
                         l += charSetInv[index]; 
                     }
             });
-            message.channel.send(m);
+            message.channel.send(normalizeLines(m).join('\n'));            
         })
 }
 
@@ -96,11 +101,47 @@ function fixLine(line) {
             if (i % 2 == 0) l[i] += '.';
             else l[i] += '..';
         }
-        if (l[i] == '{' || l[i] == '}') {
-            l[i] += '{';
-        }01
+        // if (l[i] == '{' || l[i] == '}') {
+        //     l[i] += '{';
+        // }
     }
     return l.join('');
+}
+
+function getPixWidth(line) {
+    let len = 0;
+    let l = line.split('');
+    l.forEach(letter => {
+        len += (pixWidth[charSetInv.indexOf(letter)] + 1);
+    });
+    return len;
+}
+
+function normalizeLines(l) {
+    let lines = l;
+    let ave = 0;
+    for (var j = 0; j < lines.length; j++) {
+        ave += getPixWidth(lines[j]);
+    }
+    ave = ave / lines.length;
+    for (var i = 0; i < lines.length; i++) {
+        lines[i] = bringToTarget(lines[i], ave);
+    }
+    return lines;
+}
+
+function bringToTarget(l, target) {
+    let line = l;
+    let margin = pixWidth[charSetInv.indexOf(line[line.length - 1])];
+    if (getPixWidth(line) - target >= margin) {
+        line = line.substring(0, line.length - 1);
+        bringToTarget(line, target);
+    }
+    else if ((getPixWidth(line) - target) <= (margin * -1)) {
+        line += line[line.length - 1];
+        bringToTarget(line, target);
+    }
+    return line;
 }
 
 bot.login(token);
